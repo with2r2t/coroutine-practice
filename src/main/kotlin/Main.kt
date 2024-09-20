1455a106243095626455f26ee0d5006e197939da
with2r2t <garvdl4@gmail.com>
2024-09-20 15:14:02 -0400

    project init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,20 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.*
+
+fun main() = runBlocking { // this: CoroutineScope
+    doWorld()
+    println("Done") // main coroutine continues while a previous one is delayed
+}
+
+suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    launch{
+        delay(3000L) // non-blocking delay for 1 second (default time unit is ms)
+        println("World?") // print after delay
+    }
+    println("Hello")
 }
